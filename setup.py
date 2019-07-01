76ad99f3705a878898ed52548030c7b6f0bf357d
Mathias Nedrebø <mathias@nedrebo.org>
2019-07-01 21:54:23 +0200

    Make using old PIP easier
    This change improves the diagnostics from setup.py. A new helpful
    error messages will trigger if any dependencies are missing when
    installing the package. This typically happens when PIP version is <19
    and the user is missing the build dependencies.
    
    Example error message:
    
        Missing module 'skbuild'. Please install 'scikit-build' manually
        or use PIP>=19 to handle build dependencies automatically (PEP
        517).
    
    Version number is bumped to 0.9.1 and this version will be uploaded to
    PyPI.
    
    The README.md is now simplified to not use venv since we gracefully
    handle older PIP version. It is also updated to install the package
    from PyPI and not git.
    
    Added more info to Versioning section of README.md
    
    Added PyPI link to README.md
    
    wip

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from skbuild import setup
-
+from pkgutil import iter_modules
 
 # To be replaced by: from setuptools_scm import get_version
 def get_version():
-    return "0.9.0"
+    return "0.9.1"
 
 
 def _zivid_sdk_version():
@@ -29,22 +28,46 @@ def _zivid_python_version():
     return version
 
 
-setup(
-    name="zivid",
-    version=_zivid_python_version(),
-    description="Defining the Future of 3D Machine Vision",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://www.zivid.com",
-    author="Zivid AS",
-    author_email="support@zivid.com",
-    license=open("LICENSE").read(),
-    packages=["zivid", "_zivid"],
-    package_dir={"": "modules"},
-    install_requires=["numpy"],
-    cmake_args=[
-        "-DZIVID_PYTHON_VERSION=" + _zivid_python_version(),
-        "-DZIVID_SDK_VERSION=" + _zivid_sdk_version(),
-        "-Dpybind11_DIR=src/3rd-party/pybind11-2.2.4/share/cmake/pybind11/",
-    ],
-)
+def _check_dependency(module_name, package_hint=None):
+    if module_name not in [module[1] for module in iter_modules()]:
+        raise ImportError(
+            "Missing module '{}'. Please install '{}' manually or use PIP>=19 to handle build dependencies automatically (PEP 517).".format(
+                module_name, package_hint
+            )
+        )
+
+
+def _main():
+    # This list is a duplicate of the build-system requirments in pyproject.toml.
+    # The purpose of these checks is to help users with PIP<19 lacking support for
+    # pyproject.toml
+    # Keep the two lists in sync
+    _check_dependency("skbuild", "scikit-build")
+    _check_dependency("cmake")
+    _check_dependency("ninja")
+
+    from skbuild import setup
+
+    setup(
+        name="zivid",
+        version=_zivid_python_version(),
+        description="Defining the Future of 3D Machine Vision",
+        long_description=open("README.md").read(),
+        long_description_content_type="text/markdown",
+        url="https://www.zivid.com",
+        author="Zivid AS",
+        author_email="support@zivid.com",
+        license=open("LICENSE").read(),
+        packages=["zivid", "_zivid"],
+        package_dir={"": "modules"},
+        install_requires=["numpy"],
+        cmake_args=[
+            "-DZIVID_PYTHON_VERSION=" + _zivid_python_version(),
+            "-DZIVID_SDK_VERSION=" + _zivid_sdk_version(),
+            "-Dpybind11_DIR=src/3rd-party/pybind11-2.2.4/share/cmake/pybind11/",
+        ],
+    )
+
+
+if __name__ == "__main__":
+    _main()
