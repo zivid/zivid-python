49bda4660895e232fc5dcd63fa3eb20f0be01376
Trym Bremnes <trym.bremnes@zivid.com>
2019-11-22 16:48:17 +0100

    Bump SDK version to 1.7.0

--- continuous-integration/linux/platform-dependent/ubuntu-16.04/setup.sh
+++ continuous-integration/linux/platform-dependent/ubuntu-16.04/setup.sh
@@ -50,5 +50,5 @@ function install_www_deb {
     rm -r $TMP_DIR || exit $?
 }
 
-install_www_deb https://www.zivid.com/hubfs/softwarefiles/releases/1.6.0+7a245bbe-26/u16/zivid-telicam-driver_2.0.0.1-1_amd64.deb || exit $?
-install_www_deb https://www.zivid.com/hubfs/softwarefiles/releases/1.6.0+7a245bbe-26/u16/zivid_1.6.0+7a245bbe-26_amd64.deb || exit $?
+install_www_deb https://www.zivid.com/hubfs/softwarefiles/releases/1.7.0+a115eaa4-4/u16/zivid-telicam-driver_2.0.0.1-1_amd64.deb || exit $?
+install_www_deb https://www.zivid.com/hubfs/softwarefiles/releases/1.7.0+a115eaa4-4/u16/zivid_1.7.0+a115eaa4-4_amd64.deb || exit $?
