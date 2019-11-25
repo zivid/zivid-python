95f77b5ff0d03a1ea30a3769692db7a68bfc1a13
Trym Bremnes <trym.bremnes@zivid.com>
2019-11-25 07:00:43 +0100

    Removed unnecessary pylint ignore statement

--- modules/zivid/__init__.py
+++ modules/zivid/__init__.py
@@ -5,22 +5,14 @@ import zivid._version
 
 __version__ = zivid._version.get_version(__name__)  # pylint: disable=protected-access
 
-from zivid.application import (
-    Application,  # noqa: F401 'zivid.application.Application' imported but unused
-)
-from zivid.camera import Camera  # noqa: F401 'zivid.camera.Camera' imported but unused
-from zivid.camera_state import (
-    CameraState,  # noqa: F401 'zivid.camera_state.CameraState' imported but unused
-)
 import zivid.environment
 import zivid.firmware
-from zivid.point_cloud import PointCloud
-from zivid.frame import Frame  # noqa: F401 'zivid.frame.Frame' imported but unused
-from zivid.frame_info import (
-    FrameInfo,  # noqa: F401 'zivid.frame_info.FrameInfo' imported but unused
-)
 import zivid.hdr
+from zivid.application import Application
+from zivid.camera import Camera
+from zivid.camera_state import CameraState
+from zivid.frame import Frame
+from zivid.frame_info import FrameInfo
+from zivid.point_cloud import PointCloud
 from zivid.sdk_version import SDKVersion
-from zivid.settings import (
-    Settings,  # noqa: F401 'zivid.settings.Settings' imported but unused
-)
+from zivid.settings import Settings
