ef9c9d44ffdbbd1faf9e2f25a37fe58c5c9eff13
Mathias Nedrebø <mathias.nedrebo@zividlabs.com>
2020-03-11 15:43:27 +0100

    Changing from `wget -q` to `wget -nw`
    With `-q` we don't get any output from `wget`, not even on
    errors(!). I spent too much time debugging this. With `-nv` we get
    three lines of output, which is fine, but no fancy animations
    resulting in hundreds of lines in dumb terminals like the one in the
    CI.

--- continuous-integration/linux/platform-dependent/fedora-30/setup.sh
+++ continuous-integration/linux/platform-dependent/fedora-30/setup.sh
@@ -15,7 +15,7 @@ alternatives --install /usr/bin/pip pip /usr/bin/pip3 0 || exit $?
 function install_opencl_cpu_runtime {
     TMP_DIR=$(mktemp --tmpdir --directory zivid-setup-opencl-cpu-XXXX) || exit $?
     pushd $TMP_DIR || exit $?
-    wget -q https://www.dropbox.com/s/0cvg8fypylgal2m/opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25.tgz || exit $?
+    wget -nv https://www.dropbox.com/s/0cvg8fypylgal2m/opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25.tgz || exit $?
     tar -xf opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25.tgz || exit $?
     dnf install --assumeyes opencl_runtime_*/rpm/*.rpm || exit $?
     popd || exit $?
@@ -27,7 +27,7 @@ install_opencl_cpu_runtime || exit $?
 function install_www_deb {
     TMP_DIR=$(mktemp --tmpdir --directory zivid-python-install-www-deb-XXXX) || exit $?
     pushd $TMP_DIR || exit $?
-    wget -q "$@" || exit $?
+    wget -nv "$@" || exit $?
     ar x ./*deb || exit $?
     bsdtar -xf data.tar.*z -C / || exit $?
     ldconfig || exit $?
