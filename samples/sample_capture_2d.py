f1d25002d391969e74fd234fea57b832ad421198
AzureAD\SatjaSivcev <satja.sivcev@zivid.com>
2020-03-15 09:02:56 -0400

    Change exposure values in sample_capture_2d.py
    Make the python sample consistent with the equivalent C++ sample

--- samples/sample_capture_2d.py
+++ samples/sample_capture_2d.py
@@ -8,8 +8,9 @@ def _main():
     camera = app.connect_camera()
 
     settings_2d = zivid.Settings2D()
-    settings_2d.iris = 50
-    settings_2d.exposure_time = datetime.timedelta(microseconds=50000)
+    settings_2d.exposure_time = datetime.timedelta(microseconds=10000)
+    settings_2d.gain = 1
+    settings_2d.iris = 35
 
     with camera.capture_2d(settings_2d) as frame_2d:
         image = frame_2d.image()
