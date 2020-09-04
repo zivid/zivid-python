954b9db745952d4e0394a6758680f6a0dfe9a378
Eskil Aursand <eskil.aursand@zivid.com>
2020-09-04 09:28:53 +0200

    Fix Windows build
    Windows compilers complained about some implicit casting being done
    in ReleaseableImage.cpp/ReleasableArray2D.cpp.

--- src/ReleasableImage.cpp
+++ src/ReleasableImage.cpp
@@ -10,25 +10,27 @@ namespace
     {
         using WrapperType = uint8_t;
         using NativeType = Zivid::ColorRGBA;
-        constexpr size_t dim = 3;
-        constexpr size_t depth = 4;
 
-        static_assert(sizeof(WrapperType) * depth == sizeof(NativeType));
+        constexpr py::ssize_t dim = 3;
+        constexpr py::ssize_t depth = 4;
+        constexpr py::ssize_t dataSize = sizeof(WrapperType);
+
+        static_assert(dataSize * depth == sizeof(NativeType));
         static_assert(std::is_same_v<WrapperType, decltype(NativeType::r)>);
         static_assert(std::is_same_v<WrapperType, decltype(NativeType::g)>);
         static_assert(std::is_same_v<WrapperType, decltype(NativeType::b)>);
         static_assert(std::is_same_v<WrapperType, decltype(NativeType::a)>);
 
         auto *dataPtr = static_cast<void *>(const_cast<NativeType *>(image.impl().data()));
+        const auto height = static_cast<py::ssize_t>(image.impl().height());
+        const auto width = static_cast<py::ssize_t>(image.impl().width());
 
-        return py::buffer_info{
-            dataPtr,
-            sizeof(WrapperType),
-            py::format_descriptor<WrapperType>::format(),
-            dim,
-            { image.impl().height(), image.impl().width(), depth },
-            { sizeof(WrapperType) * image.impl().width() * depth, sizeof(WrapperType) * depth, sizeof(WrapperType) }
-        };
+        return py::buffer_info{ dataPtr,
+                                dataSize,
+                                py::format_descriptor<WrapperType>::format(),
+                                dim,
+                                { height, width, depth },
+                                { dataSize * width * depth, dataSize * depth, dataSize } };
     }
 } // namespace
 
