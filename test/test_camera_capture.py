db636dadba4b0ee00ae8811ca9b4a6941bf128b8
Eskil Aursand <eskil.aursand@zivid.com>
2021-12-17 14:24:14 +0100

    Bump SDK version from 2.5.0 to 2.6.0
    This commit extends the API with the following new datamodel fields:
    - Settings.Diagnostics
    - FrameInfo.SystemInfo
    Thus, the Python API version is bumped from 2.3.0 to 2.4.0.
    
    This commit also adds unit-tests covering the new datamodel fields.

--- test/test_camera_capture.py
+++ test/test_camera_capture.py
@@ -78,3 +78,28 @@ def test_empty_settings_list(file_camera):
 
     with pytest.raises(TypeError):
         file_camera.capture([])
+
+
+def test_diagnostics_capture(file_camera):
+    from pathlib import Path
+    from tempfile import TemporaryDirectory
+    import os.path
+    import zivid
+
+    settings = zivid.Settings()
+    settings.acquisitions.append(zivid.Settings.Acquisition())
+    settings.diagnostics.enabled = False
+    frame_diagnostics_off = file_camera.capture(settings)
+    settings.diagnostics.enabled = True
+    frame_diagnostics_on = file_camera.capture(settings)
+
+    with TemporaryDirectory() as tmpdir:
+        file_diagnostics_off = Path(tmpdir) / "diagnostics_off.zdf"
+        file_diagnostics_on = Path(tmpdir) / "diagnostics_on.zdf"
+        frame_diagnostics_off.save(file_diagnostics_off)
+        frame_diagnostics_on.save(file_diagnostics_on)
+
+        # Diagnostics ON should lead to a significantly larger file size.
+        mb_diagnostics_off = os.path.getsize(str(file_diagnostics_off)) / 1e6
+        mb_diagnostics_on = os.path.getsize(str(file_diagnostics_on)) / 1e6
+        assert mb_diagnostics_on > (mb_diagnostics_off + 1.0)
