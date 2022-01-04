8c703a8e914e564a0204b3d0878e9a3d5e7aea18
Mathias Nedrebø <mathias.nedrebo@zividlabs.com>
2022-01-04 22:10:34 +0100

    Added python 3.10 testing for Linux using the Fedora 35 image

---
+++ continuous-integration/linux/platform-dependent/fedora-35/setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(realpath $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd) )"
+
+dnf --assumeyes install \
+    bsdtar \
+    clinfo \
+    g++ \
+    libatomic \
+    python3-devel \
+    python3-pip \
+    wget \
+    || exit $?
+
+alternatives --install /usr/bin/python python /usr/bin/python3 0 || exit $?
+alternatives --install /usr/bin/pip pip /usr/bin/pip3 0 || exit $?
+
+source $(realpath $SCRIPT_DIR/../common.sh) || exit $?
+# Install OpenCL CPU runtime driver prerequisites
+dnf --assumeyes install \
+    numactl-libs \
+    redhat-lsb-core \
+    || exit $?
+install_opencl_cpu_runtime || exit $?
+
+function install_www_deb {
+    TMP_DIR=$(mktemp --tmpdir --directory zivid-python-install-www-deb-XXXX) || exit $?
+    pushd $TMP_DIR || exit $?
+    wget -nv "$@" || exit $?
+    ar x ./*deb || exit $?
+    bsdtar -xf data.tar.*z -C / || exit $?
+    ldconfig || exit $?
+    popd || exit $?
+    rm -r $TMP_DIR || exit $?
+}
+
+install_www_deb https://www.zivid.com/hubfs/softwarefiles/releases/2.5.0+19fa6891-1/u20/zivid-telicam-driver_3.0.1.1-3_amd64.deb || exit $?
+install_www_deb https://www.zivid.com/hubfs/softwarefiles/releases/2.5.0+19fa6891-1/u20/zivid_2.5.0+19fa6891-1_amd64.deb || exit $?
