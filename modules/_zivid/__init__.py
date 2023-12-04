57289605be376953757bfbd235e898cb34bd0047
Torbjorn Sorby <torbjorn.sorby@zivid.com>
2023-12-04 13:07:37 +0100

    Add PixelMapping
    Add wrapper for `zivid.calibration.pixel_mapping()` as
    well as `zivid.experimental.PixelMapping`

--- modules/_zivid/__init__.py
+++ modules/_zivid/__init__.py
@@ -65,6 +65,7 @@ try:
         infield_correction,
         Matrix4x4,
         data_model,
+        PixelMapping,
         projection,
         ProjectedImage,
         presets,
---
+++ src/PixelMapping.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Zivid/Experimental/PixelMapping.h>
+
+#include <pybind11/pybind11.h>
+
+namespace ZividPython
+{
+    void wrapClass(pybind11::class_<Zivid::Experimental::PixelMapping> pyClass)
+    {
+        pyClass.def(pybind11::init(), "Initializes all values to their defaults")
+            .def(pybind11::init<int, int, float, float>())
+            .def("row_stride", &Zivid::Experimental::PixelMapping::rowStride)
+            .def("col_stride", &Zivid::Experimental::PixelMapping::colStride)
+            .def("row_offset", &Zivid::Experimental::PixelMapping::rowOffset)
+            .def("col_offset", &Zivid::Experimental::PixelMapping::colOffset);
+    }
+} // namespace ZividPython
