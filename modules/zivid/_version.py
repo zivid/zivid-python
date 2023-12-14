d237895f201816b0003be11e21ac4bd2d323faed
John Ingve Olsen <john.olsen@zivid.com>
2023-12-14 11:26:43 +0100

    Use pkg_resources in get_version for Python < 3.8
    pkg_resources was replaced with importlib.metadata in 1faf9f5 in order
    to add support for Python 3.12, which gives a deprecation warning when
    using pkg_resources. However, we want to continue supporting Python 3.7
    for now, so this commit adds a fallback to pkg_resources for Python 3.7
    or lower.

--- modules/zivid/_version.py
+++ modules/zivid/_version.py
@@ -1,8 +1,9 @@
 """Query module version."""
-from importlib.metadata import version, PackageNotFoundError
+import sys
 
 
 def get_version(module_name):
+    # pylint: disable=import-outside-toplevel
     """Return the version of a module.
 
     Args:
@@ -11,6 +12,19 @@ def get_version(module_name):
     Returns:
         The module version or None if not found.
     """
+
+    # pkg_resources is deprecated and throws a warning in newer Python verisons.
+    # We use importlib.metadata instead, but this is only available since Python 3.8.
+    if sys.version_info < (3, 8):
+        from pkg_resources import get_distribution, DistributionNotFound
+
+        try:
+            return get_distribution(module_name).version
+        except DistributionNotFound:
+            return None
+
+    from importlib.metadata import version, PackageNotFoundError
+
     try:
         return version(module_name)
     except PackageNotFoundError:
