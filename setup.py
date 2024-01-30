ecd2d13990ad477452f27377dc229b53b958889b
Eskil Aursand <eskil.aursand@gmail.com>
2024-01-30 11:12:20 +0100

    WIP

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ from skbuild import setup, constants
 
 # To be replaced by: from setuptools_scm import get_version
 def get_version():
-    return "2.11.0"
+    return "2.12.0"
 
 
 def _zivid_sdk_version():
-    return "2.11.0"
+    return "2.12.0"
 
 
 def _zivid_python_version():
