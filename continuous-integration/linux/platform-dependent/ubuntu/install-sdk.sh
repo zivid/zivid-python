aa0bc041ca8fdf35e1dfefaee2d1125795516e76
John Ingve Olsen <john.olsen@zivid.com>
2024-06-17 11:24:39 +0200

    Bump to SDK 2.13.0-rc-2

--- continuous-integration/linux/platform-dependent/ubuntu/install-sdk.sh
+++ continuous-integration/linux/platform-dependent/ubuntu/install-sdk.sh
@@ -9,4 +9,4 @@ function ubuntu_install_www_deb {
     rm -r $TMP_DIR || exit $?
 }
 
-ubuntu_install_www_deb "https://downloads.zivid.com/sdk/releases/${ZIVID_SDK_EXACT_VERSION}/u20/zivid_${ZIVID_SDK_EXACT_VERSION}_amd64.deb" || exit $?
+ubuntu_install_www_deb "https://downloads.zivid.com/sdk/previews/${ZIVID_SDK_EXACT_VERSION}/u20/amd64/zivid_${ZIVID_SDK_EXACT_VERSION}_amd64.deb" || exit $?
