32546021910d1619dacb399c24fb0cfb73bba6aa
John Ingve Olsen <john.olsen@zivid.com>
2025-03-07 16:15:30 +0100

    Bump to Zivid SDK 2.15
    -   Updates low DoF hand-eye transform reference data to fix failing
        test.
    
    -   Fixes pixel mapping test. In SDK 2.15, the experimental pixel
        mapping considers the 2D resolution implied by the settings when
        returning the mapping. The settings tested implied that 2D and 3D
        had the same resolution. Change so that 2D implies full resolution,
        thus returning the same pixel mapping as in SDK 2.14.
    
    -   Update handeye test data. The test data was failing with SDK 2.15
    
    -   Update marker detection test data. The marker detection in SDK 2.15
        gives slightly different results from SDK 2.14. Update the test data
        to make tests pass.
    
    -   Update Windows installer setup arguments

--- test/experimental/test_pixel_mapping.py
+++ test/experimental/test_pixel_mapping.py
@@ -1,6 +1,6 @@
 def test_pixel_mapping(file_camera):
     from zivid.experimental.calibration import pixel_mapping
-    from zivid.settings import Settings
+    from zivid import Settings, Settings2D
 
     pixel_mapping_handle = pixel_mapping(
         camera=file_camera, settings=Settings(acquisitions=[Settings.Acquisition()])
@@ -14,9 +14,13 @@ def test_pixel_mapping(file_camera):
     assert pixel_mapping_handle.row_offset == 0.0
     assert pixel_mapping_handle.col_offset == 0.0
 
+    full_resolution_2d_settings = Settings2D()
+    full_resolution_2d_settings.acquisitions.append(Settings2D.Acquisition())
+
     blue_subsample2x2_settings = Settings()
     blue_subsample2x2_settings.acquisitions.append(Settings.Acquisition())
     blue_subsample2x2_settings.sampling.pixel = Settings.Sampling.Pixel.blueSubsample2x2
+    blue_subsample2x2_settings.color = full_resolution_2d_settings
     pixel_mapping_handle = pixel_mapping(
         camera=file_camera, settings=blue_subsample2x2_settings
     )
@@ -28,6 +32,7 @@ def test_pixel_mapping(file_camera):
     red_subsample2x2_settings = Settings()
     red_subsample2x2_settings.acquisitions.append(Settings.Acquisition())
     red_subsample2x2_settings.sampling.pixel = Settings.Sampling.Pixel.redSubsample2x2
+    red_subsample2x2_settings.color = full_resolution_2d_settings
     pixel_mapping_handle = pixel_mapping(
         camera=file_camera, settings=red_subsample2x2_settings
     )
